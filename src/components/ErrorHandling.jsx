db873668f33197440e405fe6e2d214b1ec389022
Craig <craigtipple81@gmail.com>
2023-12-10 12:35:17 +0000

    Added an ErrorHandling component to cover incorrect URLs

--- src/components/ErrorHandling.jsx
+++ src/components/ErrorHandling.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function ErrorHandling() {
-  return <div className="container"></div>;
+  const navigate = useNavigate();
+  return (
+    <section className="d-flex align-items-center justify-content-center">
+      <div
+        className="d-flex-col text-center"
+        style={{ height: "500px", width: "500px" }}
+      >
+        <h3>Oops, we've taken a wrong turn...</h3>
+        <p>Click the button to return Home</p>
+        <button
+          onClick={() => {
+            navigate("/home");
+          }}
+          className="btn btn-success"
+        >
+          Home
+        </button>
+      </div>
+    </section>
+  );
 }
