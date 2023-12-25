fd01bbdb6cfb0a4cf35e19a68746c459d8e9f598
Craig <craigtipple81@gmail.com>
2023-12-25 19:12:56 +0000

    Updated profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ export default function Header() {
   return (
     <header
       className="d-flex flex-md-row justify-content-between align-items-center border-bottom border-success-subtle mb-4"
-      style={{ height: "8rem" }}>
+      style={{ height: "8rem" }}
+    >
       <h1 className="display-5 text-success m-4">Agrarian</h1>
 
       {user ? (
@@ -26,12 +27,14 @@ export default function Header() {
           <div className="d-flex flex-column flex-md-row mx-4">
             <button
               onClick={() => navigate("/login")}
-              className="btn btn-success m-2">
+              className="btn btn-success m-2"
+            >
               Login
             </button>
             <button
               onClick={() => navigate("/register")}
-              className="btn btn-outline-success m-2">
+              className="btn btn-outline-success m-2"
+            >
               Register
             </button>
           </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -22,53 +22,52 @@ export default function Profile() {
 
   return (
     <>
-      <div
-        className="container"
-        style={{ width: "12rem" }}>
+      <div className="container" style={{ width: "12rem" }}>
+        <div className="text-center" style={{ width: "auto" }}>
+          <p style={{ fontWeight: "bold" }}>{user.user_name}</p>
+        </div>
         <div className="dropdown text-center">
           <Dropdown>
-            <Dropdown.Toggle
-              className="bg-success"
-              id="dropdownMenuButton">
+            <Dropdown.Toggle className="bg-success" id="dropdownMenuButton">
               <i className="fa-solid fa-user"></i>
             </Dropdown.Toggle>
             <Dropdown.Menu>
               <Dropdown.Item
                 className="text-success"
                 onClick={() => navigate("/")}
-                style={{ backgroundColor: "transparent" }}>
+                style={{ backgroundColor: "transparent" }}
+              >
                 Home
               </Dropdown.Item>
 
               <Dropdown.Item
                 className="text-success"
                 onClick={() => navigate("/exchange")}
-                style={{ backgroundColor: "transparent" }}>
+                style={{ backgroundColor: "transparent" }}
+              >
                 Exchange
               </Dropdown.Item>
 
               <Dropdown.Item
                 className="text-success"
                 onClick={() => navigate("/posts")}
-                style={{ backgroundColor: "transparent" }}>
+                style={{ backgroundColor: "transparent" }}
+              >
                 Posts
               </Dropdown.Item>
 
               <Dropdown.Item
                 className="text-success"
                 onClick={() => navigate("/messenger")}
-                style={{ backgroundColor: "transparent" }}>
+                style={{ backgroundColor: "transparent" }}
+              >
                 Messenger
               </Dropdown.Item>
 
-              <Dropdown.Item
-                className="text-danger"
-                onClick={handleLogout}>
+              <Dropdown.Item className="text-danger" onClick={handleLogout}>
                 Logout
               </Dropdown.Item>
-              <Dropdown.Item
-                className="text-danger"
-                onClick={handleDelete}>
+              <Dropdown.Item className="text-danger" onClick={handleDelete}>
                 Delete Account
               </Dropdown.Item>
             </Dropdown.Menu>
