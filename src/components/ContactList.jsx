3d4ed84faa8c215e77e9d68b80a8bcc130173a34
Craig <craigtipple81@gmail.com>
2024-01-06 16:18:39 +0000

    Imported app.css to ContactList and styled individual contact-list-items. Removed the dedicated contact-list-item component.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect } from "react";
 import { useAuth } from "./AuthContext";
+import "../App.css";
 
 export default function ContactList({ conversations }) {
   const { user } = useAuth();
 
   return (
     <div>
-      {conversations.map((conversation) =>
+      {conversations.map((conversation, index) =>
         conversation.user1_id === user.user_id ? (
-          <div key={conversation.conversation_id}>
-            {conversation.user2_username}
+          index % 2 === 0 ? (
+            <div key={index} className="bg-success-subtle contact-list-item">
+              {conversation.user2_username}
+            </div>
+          ) : (
+            <div key={index} className="bg-secondary-subtle contact-list-item">
+              {conversation.user2_username}
+            </div>
+          )
+        ) : index % 2 === 0 ? (
+          <div key={index} className="bg-success-subtle contact-list-item">
+            {conversation.user1_username}
           </div>
         ) : (
-          <div key={conversation.conversation_id}>
+          <div key={index} className="bg-secondary-subtle contact-list-item">
             {conversation.user1_username}
           </div>
         )
--- src/components/ContactListItem.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ContactListItem() {
-  return <p>List Item works!</p>;
-}
