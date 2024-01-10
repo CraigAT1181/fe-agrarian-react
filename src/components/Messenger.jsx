54acc257374b92ab64bf4c6a2e738bd0e62361db
Craig <craigtipple81@gmail.com>
2024-01-10 11:00:03 +0000

    Added a message and buttons visible to a user on the Messenger page, but who has not yet started any conversations. Buttons navigate to Exchange and Posts.

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -4,6 +4,7 @@ import ContactList from "./ContactList";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 import { getConversationsByUserID } from "../api/api";
+import { useNavigate } from "react-router-dom";
 
 export default function Messenger() {
   const [isLoading, setIsLoading] = useState(true);
@@ -12,6 +13,8 @@ export default function Messenger() {
   const [conversations, setConversations] = useState([]);
   const [conversationID, setConversationID] = useState();
   const [messageSent, setMessageSent] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsLoading(true);
@@ -60,14 +63,38 @@ export default function Messenger() {
         <div className="col-md-8 h-100">
           {conversations.length > 0 ? (
             <div>
-              <MessageList conversationID={conversationID} messageSent={messageSent} />
+              <MessageList
+                conversationID={conversationID}
+                messageSent={messageSent}
+              />
             </div>
           ) : (
-            <div>You've not contacted anyone yet.</div>
+            <div className="text-center align-items-center w-50 p-3">
+              <p>
+                Looks like you've not begun a conversation yet. To view what
+                people have posted, or find growers near you, click below.
+              </p>
+
+              <button
+                onClick={() => navigate("/exchange")}
+                className="btn btn-success mx-1 fw-bold">
+                Exchange
+              </button>
+              <button
+                onClick={() => navigate("/posts")}
+                className="btn btn-success mx-1 fw-bold">
+                Posts
+              </button>
+            </div>
           )}
-          <div className="input-box-container h-25">
-            <MessageInput conversationID={conversationID} setMessageSent={setMessageSent}/>
-          </div>
+          {conversations.length > 0 && (
+            <div className="input-box-container h-25">
+              <MessageInput
+                conversationID={conversationID}
+                setMessageSent={setMessageSent}
+              />
+            </div>
+          )}
         </div>
       </div>
     </section>
