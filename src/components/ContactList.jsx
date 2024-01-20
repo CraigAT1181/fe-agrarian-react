97fc42f0504ecd079a1ed9769a0b83a5fb478df8
Craig <craigtipple81@gmail.com>
2024-01-20 11:28:33 +0000

    Extracted useEffect with getConversationsByUserID from Messenger and put into ContactList. Created CSS .contact-list-name to ensure that long names are wrapped in the contact-list-item box.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,54 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "./AuthContext";
+import { getConversationsByUserID } from "../api/api";
 import "../App.css";
+import { useNavigate } from "react-router-dom";
 
-export default function ContactList({ conversations, setConversationID }) {
+export default function ContactList({ setConversationID }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [conversations, setConversations] = useState([]);
   const { user } = useAuth();
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    if (user) {
+      getConversationsByUserID(user.user_id)
+        .then(({ conversations }) => {
+          setIsLoading(false);
+          setConversations(conversations);
+        })
+        .catch(
+          ({
+            response: {
+              status,
+              data: { message },
+            },
+          }) => {
+            setIsLoading(false);
+            setError({ status, message: message });
+          }
+        );
+    } else {
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  if (isLoading) return <p>Just a moment...</p>;
+  if (error)
+    return (
+      <p>
+        Error {error.status} {error.message}
+      </p>
+    );
+
   return (
-    <div>
-      {conversations &&
+    <div className="d-flex-col justify-content-center">
+      {conversations.length > 0 ? (
         conversations.map((conversation, index) =>
           conversation.user1_id === user.user_id ? (
-            index % 2 === 0 ? (
-              <div
-                key={index}
-                className="bg-dark-subtle contact-list-item"
-                onClick={() => {
-                  setConversationID(conversation.conversation_id);
-                }}>
-                <div>{conversation.user2_username}</div>
-              </div>
-            ) : (
-              <div
-                key={index}
-                className="bg-secondary-subtle contact-list-item"
-                onClick={() => {
-                  setConversationID(conversation.conversation_id);
-                }}>
-                <div>{conversation.user2_username}</div>
-              </div>
-            )
-          ) : index % 2 === 0 ? (
             <div
               key={index}
-              className="bg-dark-subtle contact-list-item"
+              className="contact-list-item rounded text-success"
               onClick={() => {
                 setConversationID(conversation.conversation_id);
               }}>
-              <div>{conversation.user1_username}</div>
+              <p className="p-4 m-0 contact-list-name">{conversation.user2_username}</p>
             </div>
           ) : (
             <div
               key={index}
-              className="bg-secondary-subtle contact-list-item"
+              className="contact-list-item rounded text-success"
               onClick={() => {
                 setConversationID(conversation.conversation_id);
               }}>
-              <div>{conversation.user1_username}</div>
+              <p className="p-4 m-0 contact-list-name">{conversation.user1_username}</p>
             </div>
           )
-        )}
+        )) : 
+        
+        <div className="text-center align-items-center w-50 p-3">
+        <p>
+          Looks like you've not begun a conversation yet. To view what
+          people have posted, or find growers near you, click below.
+        </p>
+
+        <button
+          onClick={() => navigate("/exchange")}
+          className="btn btn-success mx-1 fw-bold">
+          Exchange
+        </button>
+        <button
+          onClick={() => navigate("/posts")}
+          className="btn btn-success mx-1 fw-bold">
+          Posts
+        </button>
+      </div>}
     </div>
   );
 }
--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,53 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useAuth } from "./AuthContext";
+import React, { useState } from "react";
 import ContactList from "./ContactList";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
-import { getConversationsByUserID } from "../api/api";
-import { useNavigate } from "react-router-dom";
 
 export default function Messenger() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { user } = useAuth();
-  const [conversations, setConversations] = useState([]);
-  const [conversationID, setConversationID] = useState();
+  const [conversationID, setConversationID] = useState(null);
   const [messageSent, setMessageSent] = useState(false);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    if (user) {
-      getConversationsByUserID(user.user_id)
-        .then(({ conversations }) => {
-          setIsLoading(false);
-          setConversations(conversations);
-        })
-        .catch(
-          ({
-            response: {
-              status,
-              data: { message },
-            },
-          }) => {
-            setIsLoading(false);
-            setError({ status, message: message });
-          }
-        );
-    } else {
-      setIsLoading(false);
-    }
-  }, [user]);
-
-  if (isLoading) return <p>Just a moment...</p>;
-  if (error)
-    return (
-      <p>
-        Error {error.status} {error.message}
-      </p>
-    );
 
   return (
     <section
@@ -55,39 +13,18 @@ export default function Messenger() {
       style={{ minHeight: "100vh" }}>
       <div className="row h-100">
         <div className="col-md-4 h-100">
-          <ContactList
-            conversations={conversations}
-            setConversationID={setConversationID}
-          />
+          <ContactList setConversationID={setConversationID} />
         </div>
         <div className="col-md-8 h-100">
-          {conversations.length > 0 ? (
+          {conversationID && (
             <div>
               <MessageList
                 conversationID={conversationID}
                 messageSent={messageSent}
               />
             </div>
-          ) : (
-            <div className="text-center align-items-center w-50 p-3">
-              <p>
-                Looks like you've not begun a conversation yet. To view what
-                people have posted, or find growers near you, click below.
-              </p>
-
-              <button
-                onClick={() => navigate("/exchange")}
-                className="btn btn-success mx-1 fw-bold">
-                Exchange
-              </button>
-              <button
-                onClick={() => navigate("/posts")}
-                className="btn btn-success mx-1 fw-bold">
-                Posts
-              </button>
-            </div>
           )}
-          {conversations.length > 0 && (
+          {conversationID && (
             <div className="input-box-container h-25">
               <MessageInput
                 conversationID={conversationID}
