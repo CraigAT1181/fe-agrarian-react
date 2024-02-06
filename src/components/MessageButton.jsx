062d3055bbecee761bf5fd9488069358a5a08bcc
Craig <craigtipple81@gmail.com>
2024-02-06 10:13:16 +0000

    Refactored MessageButton

--- src/components/MessageButton.jsx
+++ src/components/MessageButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 import { addConversationByUserID, getConversationsByUserID } from "../api/api";
@@ -6,38 +6,48 @@ import { addConversationByUserID, getConversationsByUserID } from "../api/api";
 export default function MessageButton({ partner }) {
   const navigate = useNavigate();
   const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleClick = () => {
-    getConversationsByUserID(user.userID)
-      .then(({ conversations }) => {
-        if (conversations.length > 0) {
-          conversations.map((conversation) => {
-            if (
-              (user.userID === conversation.user1_id ||
-                user.userID === conversation.user2_id) &&
-              (partner === conversation.user1_id ||
-                partner === conversation.user2_id)
-            ) {
-              navigate("/messenger");
-            } else {
-              addConversationByUserID(user.userID, partner).then((response) => {
-                navigate("/messenger");
-              });
-            }
-          });
-        } else {
-          addConversationByUserID(user.userID, partner).then((response) => {
-            navigate("/messenger");
-          });
-        }
-      })
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      const { conversations } = await getConversationsByUserID(user.userID);
+
+      const conversationExists = conversations.some(
+        (conversation) =>
+          (user.userID === conversation.user1_id ||
+            user.userID === conversation.user2_id) &&
+          (partner === conversation.user1_id ||
+            partner === conversation.user2_id)
+      );
+
+      if (conversationExists) {
+        navigate("/messenger");
+      } else {
+        await addConversationByUserID(user.userID, partner);
+        navigate("/messenger");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.response?.data.message || "An error occurred");
+    }
   };
+
+  if (isLoading)
+    return (
+      <div className="d-flex-col text-center mt-4">
+        <i className="fa-solid fa-spinner fa-spin"></i>
+        <p>Handling conversations...</p>
+      </div>
+    );
 
   return (
     <button
       className="btn btn-success mt-2"
       style={{ width: "8rem" }}
-      onClick={handleClick}>
+      onClick={handleClick}
+      disabled={isLoading}>
       Message
     </button>
   );
