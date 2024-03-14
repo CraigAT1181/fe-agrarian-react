1f70eb3ca825144810dfdb80e682394ff74ef179
Craig <craigtipple81@gmail.com>
2024-03-14 15:02:16 +0000

    Refactored CSS on contact list to highlight active conversation better.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,16 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useAuth } from "./AuthContext";
 import { deleteConversation, getConversationsByUserID } from "../api/api";
 import "../App.css";
 
-
-export default function ContactList({ conversations, setConversations, setConversationID }) {
+export default function ContactList({
+  conversations,
+  setConversations,
+  setConversationID,
+}) {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [conversationDeleted, setConversationDeleted] = useState(false);
   const { user } = useAuth();
-
- 
+  const topItemRef = useRef(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -38,6 +40,14 @@ export default function ContactList({ conversations, setConversations, setConver
 
     setConversationDeleted(false);
   }, [user, conversationDeleted]);
+
+  useEffect(() => {
+    // Focus on the top item when the component mounts
+    if (topItemRef.current) {
+      topItemRef.current.focus();
+      setConversationID(conversations[0].conversation_id);
+    }
+  }, [conversations, setConversationID]);
 
   const handleDeleteConversation = (userID, conversationID) => {
     setIsLoading(true);
@@ -79,59 +89,73 @@ export default function ContactList({ conversations, setConversations, setConver
 
   return (
     <div
-      className="d-flex-col justify-content-center"
+      className="d-flex-col justify-content-center p-2"
       style={{ overflowY: "auto" }}>
-      {conversations.length > 0 ? (
-        conversations.map((conversation, index) =>
-          conversation.user1_id === user.userID ? (
-            <div
-              key={index}
-              className="contact-list-item rounded text-success">
+      {conversations.length > 0
+        ? conversations.map((conversation, index) =>
+            conversation.user1_id === user.userID ? (
               <div
-                className="contact-list-name"
+                key={index}
+                className="contact-list-item rounded text-success"
+                ref={index === 0 ? topItemRef : null}
+                tabIndex={index === 0 ? 0 : -1}
                 onClick={() => {
                   setConversationID(conversation.conversation_id);
                 }}>
-                {conversation.user2_username}
-              </div>
-              <div className="position-absolute top-0 end-0 m-1 ">
-                <span
-                  className="badge bg-success-subtle"
-                  style={{ cursor: "pointer" }}
+                <div
+                  className="contact-list-name"
                   onClick={() => {
-                    handleDeleteConversation(user.userID, conversation.conversation_id);
+                    setConversationID(conversation.conversation_id);
                   }}>
-                  X
-                </span>
+                  {conversation.user2_username}
+                </div>
+                <div className="position-absolute top-0 end-0 m-1 ">
+                  <span
+                    className="badge bg-success-subtle"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      handleDeleteConversation(
+                        user.userID,
+                        conversation.conversation_id
+                      );
+                    }}>
+                    X
+                  </span>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div
-              key={index}
-              className="contact-list-item rounded text-success">
+            ) : (
               <div
-                className="contact-list-name"
+                key={index}
+                className="contact-list-item rounded text-success"
+                ref={index === 0 ? topItemRef : null}
+                tabIndex={index === 0 ? 0 : -1}
                 onClick={() => {
                   setConversationID(conversation.conversation_id);
                 }}>
-                {conversation.user1_username}
-              </div>
-              <div className="position-absolute top-0 end-0 m-1 ">
-                <span
-                  className="badge bg-success-subtle"
-                  style={{ cursor: "pointer" }}
+                <div
+                  className="contact-list-name"
                   onClick={() => {
-                    handleDeleteConversation(user.userID, conversation.conversation_id);
+                    setConversationID(conversation.conversation_id);
                   }}>
-                  X
-                </span>
+                  {conversation.user1_username}
+                </div>
+                <div className="position-absolute top-0 end-0 m-1 ">
+                  <span
+                    className="badge bg-success-subtle"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                      handleDeleteConversation(
+                        user.userID,
+                        conversation.conversation_id
+                      );
+                    }}>
+                    X
+                  </span>
+                </div>
               </div>
-            </div>
+            )
           )
-        )
-      ) : (
-        null
-      )}
+        : null}
     </div>
   );
 }
