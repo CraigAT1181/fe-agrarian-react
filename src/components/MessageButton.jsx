1594181ebdf26de788f741ce03e4c6734707401b
Craig <craigtipple81@gmail.com>
2024-07-30 09:47:50 +0100

    MessageButton removed console log

--- src/components/MessageButton.jsx
+++ src/components/MessageButton.jsx
@@ -4,7 +4,6 @@ import { useAuth } from "./AuthContext";
 import { addConversationByUserID, getConversationsByUserID } from "../api/api";
 
 export default function MessageButton({ partner, size, colour }) {
-  console.log(partner, size, colour);
   const navigate = useNavigate();
   const { user } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
